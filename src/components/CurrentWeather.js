0c2105e3b744c7399210b470c718970f74304147
Jesse McFadden <jessemcbrennan@gmail.com>
2018-07-17 14:39:00 -0600

    Add Current Weather Component and Pass Props

---
+++ src/components/CurrentWeather.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const CurrentWeather = (props) => {
+  return (
+    <div>
+      <h1>Current Weather in {props.city}</h1>
+      <p> {props.currentCondition} </p>
+      <p> {props.currentTemp} </p>
+      <p> {props.expHigh} </p>
+      <p> {props.expLow} </p>
+      <p> {props.summary} </p>
+
+    </div>
+    )
+}
+
+export default CurrentWeather;
+
+
+
+   //        city={city} 
+   //        currentCondition={currentCondition}  
+   //        currentTemp={currentTemp}
+   //        expHigh={expHigh}
+   //        expLow={expLow}
+   //        summar={summary}
+   // 
