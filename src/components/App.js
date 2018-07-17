7a0876a2eb5ab8a026dbd545730a4166e20eb52a
Jesse McFadden <jessemcbrennan@gmail.com>
2018-07-17 15:05:57 -0600

    Add mock data helper file

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import '../css/App.css';
 import CurrentWeather from './CurrentWeather'
 
-let fakeData = {
-  city: 'Denver',
-  state: 'CO',
-  currentCondition: 'Sunny',
-  currentDay: 'Tuesday',
-  currentTemp: 90, 
-  expHigh: 98,
-  expLow: 68,
-  summary: 'Windy with a chance of late night showers'
-}
+// let fakeData = {
+//   city: 'Denver',
+//   state: 'CO',
+//   currentCondition: 'Sunny',
+//   currentDay: 'Tuesday',
+//   currentTemp: 90, 
+//   expHigh: 98,
+//   expLow: 68,
+//   summary: 'Windy with a chance of late night showers'
+// }
 
 
 class App extends Component {
---
+++ src/utils/helpers.js
@@ -0,0 +1,3 @@
+const mockData = require("./mockData")
+
+console.log(mockData.current_observation.display_location.full  )
