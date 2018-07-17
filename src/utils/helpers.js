70bbf9d8d4ac65c6dac10f38cd5e37d711226969
Jesse McFadden <jessemcbrennan@gmail.com>
2018-07-17 15:26:32 -0600

    Add mock city data to helper

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,7 @@
 const mockData = require("./mockData")
 
-console.log(mockData.current_observation.display_location.full  )
+// console.log(mockData.current_observation.display_location.full  )
+
+const city = mockData.current_observation.display_location.city
+
+const currentCondition = mockData.current
