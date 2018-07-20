d24cd273d5b1d1dfbd5370e275173c2d80d4ba09
Jesse McFadden <jessemcbrennan@gmail.com>
2018-07-19 18:29:47 -0600

    Add work test suit

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,9 +1,43 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { shallow, mount } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import App from '../components/App';
+
+describe('App', () => {
+
+  it('should exist', () => {
+
+  });
+
+  it('should have an initial state', () => {
+
+  });
+
+  it('should render CurrentWeather component', () => {
+
+  });
+
+  it('should render a Search component', () => {
+
+  });
+
+  it('should render a SevenDay component', () => {
+
+  })
+
+  it('should render a TenDay component', () => {
+
+  })
+
+  it('should set a location for weather data', () => {
+
+  })
+
+  it('should render a welcome page if no location has been set', () => {
+
+  })
+
+  it('should persist location in local storage', () => {
+
+  })
 });
