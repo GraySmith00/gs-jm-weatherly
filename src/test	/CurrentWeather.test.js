344c85586ddb94271682859a36d33414e7860aa2
Jesse McFadden <jessemcbrennan@gmail.com>
2018-07-25 19:59:41 -0600

    Add working CurrentWeather test

--- src/test	/CurrentWeather.test.js
+++ src/test	/CurrentWeather.test.js
@@ -4,13 +4,41 @@ import { shallow, mount } from 'enzyme';
 import CurrentWeather from '../components/CurrentWeather';
 
 describe('CurrentWeather', () => {
+  let wrapper;
 
-  it('should exist', () => {
+  const currentWeatherData = {
+    city: 'Denver',
+    currentCondition: 'Clear',
+    currentDay: 'Wednesday',
+    currentTempF: '97',
+    expHigh: '104',
+    expLow: '79',
+    icon:'clear',
+    state:'CO',
+    summary: 'Clear, Low 79C'
+  }
 
+  beforeEach(() => {
+    wrapper = shallow(<CurrentWeather currentWeather={currentWeatherData}  />)
   })
 
-  it('should CurrentWeather html', () => {
+  it('should exist', () => {
+    expect(wrapper).toBeDefined();
+  })
 
+  it('should render with the correct HTML', () => {
+    const location = wrapper.find('.current-weather > h1');
+    const currentTemp = wrapper.find('.current-info > h3');
+    const currentHigh = wrapper.find('.high-temp');
+    const currentLow = wrapper.find('.low-temp');
+    const currentImg = wrapper.find('.current-info > img')
+
+    expect(location).toBeDefined();
+    expect(location.text()).toEqual('Denver, CO');
+    expect(currentTemp.text()).toEqual('97');
+    expect(currentHigh.text()).toEqual('104');
+    expect(currentLow.text()).toEqual('79');
+    expect(currentImg).toBeDefined();
   })
 })
 
